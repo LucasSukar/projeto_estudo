22554ca1b3cfeb54d7e06888d7a1ecd55275c276
lucassukar <lfsw@cesar.school>
2025-06-26 13:36:10 -0300

    feat: cria index e update no postController

--- src/controllers/PostControllers.js
+++ src/controllers/PostControllers.js
@@ -4,13 +4,49 @@ class PostController {
   async create(req, res){
     try{
       const novoPost = await Post.create(req.body)
-      res.json(novoPost)
+      return res.json(novoPost)
     }catch(e){
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message)
       })
     }
   }
-}
 
+  async index(req, res){
+    try{
+      const posts = await Post.findAll()
+      return res.json(posts)
+    } catch(e){
+      return res.json(null)
+    }
+  }
+
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não informado para atualização']
+        });
+      }
+
+      const post = await Post.findByPk(id);
+
+      if (!post) {
+        return res.status(404).json({
+          errors: ['Post não encontrado']
+        });
+      }
+
+      await post.update(req.body);
+
+      return res.json(post); // retorna o post atualizado
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ error: 'Erro ao atualizar o post' });
+    }
+  }
+}
 export default new PostController()
--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -3,5 +3,7 @@ const router = new Router()
 import PostController from '../controllers/PostControllers'
 
 router.post('/', PostController.create)
+router.get('/', PostController.index)
+router.put('/:id', PostController.update)
 
 export default router
